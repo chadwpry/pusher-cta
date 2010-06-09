46b1a7b30c8d7e1eaeae274e354a2f1ceaf2541a
Chad Pry <chad.pry@gmail.com>
2010-06-09 15:06:04 -0500

    updated javascript libraries and display on page to swap out markers on refreshes

--- public/javascripts/howtopush.js
+++ public/javascripts/howtopush.js
@@ -1,18 +1,24 @@
 var map = null;
 var socket = null;
+var marker_list = [];
 
 function setup_socket(api_key, channel) {
   socket = new Pusher(api_key, channel);
 
   socket.bind('location_move', function(data) {
-		// Create a new item
     var elm = '<li style="display:none;" class="new"><p class="time">'+data.vehicle.timestamp+'</p><div class="close"></div><p class="title">Vehicle: '+data.vehicle.id+'</p><p class="location"><a href="http://maps.google.com/maps?hl=en&amp;q='+data.vehicle.lat+','+data.vehicle.lon+'" target="_blank">'+data.vehicle.lat+','+data.vehicle.lon+'</a></p><p class="content">'+data.vehicle.destination+'</p></li>';
+
+    existing = marker_list[data.vehicle.id];
+    if (existing != null && (existing.lat != data.vehicle.lat || existing.lon != data.vehicle.lon)) {
+      clearMarkers([existing.marker]);
+    }
 
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(data.vehicle.lat, data.vehicle.lon),
       map: map,
       title:"Hello World!"
-    }); 
+    });
+    marker_list[data.vehicle.id] = { 'lat': data.vehicle.lat, 'lon': data.vehicle.lon, 'marker': marker };
     
     // Add the item to the list
     $('.pushes ul').prepend(elm);
@@ -43,16 +49,26 @@ function define_map() {
   return new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 }
 
+function clearMarkers(list) {
+  for (i in list) {
+    if (list[i] != null) {
+      list[i].setMap(null);
+    }
+  }
+}
+
 $(document).ready(function() {
   map = define_map();
 
   $('#eventButton').click(function() {
-    request('/vehicle_routes/' + $('#vehicle_route').val() + '/ping.js')
+    request('/vehicle_routes/' + $('#vehicle_route').val() + '/ping.js');
   })
 
-  $('#vehicle_route').change(function() {
-    $('#eventButton').attr('style', 'display:block');
+  $('#clearButton').click(function() {
+    clearMarkers(marker_list);
+  });
 
+  $('#vehicle_route').change(function() {
     $('.pushes ul').children().each(function() {
       $(this).remove();
     })
